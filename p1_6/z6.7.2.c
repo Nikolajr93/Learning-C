2e7f164b85ef1d59149e521dddd5fef42d0b18b4
Nikolajr93 <nikolajr93@gmail.com>
2018-03-28 19:45:48 +0200

    Again

---
+++ p1_6/z6.7.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int levak,i,j;
+    double moduo,t;
+    typedef struct complex {
+        double re, im;
+    }Complex;
+    Complex niz[10];
+    for(i=0; i<2; i++){
+        scanf("%lf%lf",&niz[i].re,&niz[i].im);
+        t=sqrt(niz[i].re*niz[i].re+niz[i].im*niz[i].im);
+        if(t>moduo){
+            moduo=t;
+            j=i;
+        }
+    }
+    printf("%lf\n%d",moduo,j+1);
+    scanf("%d",&levak);
+    return 0;
+}
